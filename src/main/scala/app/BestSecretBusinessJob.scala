00c137cf08813ada982f7f2ce29258ea9d142f0a
Connie Ko <cko@CHIC02T60YBH03Q.grubhub.local>
2018-04-30 13:46:33 -0400

    Add more jobs

---
+++ src/main/scala/app/BestSecretBusinessJob.scala
@@ -0,0 +1,33 @@
+package app
+
+import model.{Business, Tip}
+
+object BestSecretBusinessJob extends BaseSparkApp {
+
+  import spark.implicits._
+
+  def main(args: Array[String]): Unit = {
+
+    if (args.nonEmpty) {
+      projectPath = args(0)
+    }
+    val keyword = args(1)
+
+    lazy val business = loadData[Business]("business.json").rdd.keyBy(_.business_id)
+    val tip = loadData[Tip]("tip.json").rdd.filter(_.text.toLowerCase.contains(keyword.toLowerCase())).keyBy(_.business_id)
+
+    val result = business.cogroup(tip) filter { case(_, (b, t)) =>
+      b.filter(_.categories.map(_.toLowerCase).contains(keyword.toLowerCase())).nonEmpty ||
+      t.nonEmpty
+    } flatMap {
+      _._2._1 map { b =>
+        ((b.city, b.state), (b.business_id, b.stars))
+      }
+    } groupByKey() mapValues  { v =>
+      v.toSeq.sortBy(_._2)(Ordering[Double].reverse).take(10)
+    }
+
+    result.collect.foreach(println)
+
+  }
+}
---
+++ src/main/scala/app/DifferentFromFriends.scala
@@ -0,0 +1,65 @@
+package app
+
+import app.SparkApp.{loadData, projectPath, spark}
+import model._
+
+object DifferentFromFriends extends BaseSparkApp {
+
+  import spark.implicits._
+
+  def main(args: Array[String]): Unit = {
+
+    if(args.nonEmpty) {
+      projectPath = args(0)
+    }
+
+    println("test")
+
+    lazy val user = loadData[User]("user.json")
+    lazy val usersWithFriends = user filter {
+      _.friends.nonEmpty
+    }
+    usersWithFriends.cache
+    val targetedUsers = usersWithFriends.map(_.user_id).collect
+    lazy val reviews = loadData[Review]("review.json").filter(r => targetedUsers.contains(r.user_id))
+
+    val avgStars = reviews
+      .groupByKey(r => r.user_id)
+      .mapGroups { case (id, res) =>
+        (id, res.toSeq
+          .groupBy(_.business_id)
+          .mapValues(r => r.map(_.stars).sum.toDouble / (r.size.toDouble + 1))
+          .map(identity))
+      }.rdd.collectAsMap.toMap
+    lazy val friends = usersWithFriends.rdd.map(u => (u.user_id, u.friends))
+    val diffFromFriends = friends map { case (id, friends) =>
+      val selfReviews = avgStars.getOrElse(id, Map.empty[String, Double])
+      val friendReviews:Map[String, Double] = friends map { f =>
+        avgStars.getOrElse(f, Map.empty[String, Double])
+      } reduce { (a, b) =>
+        (a.keySet ++ b.keySet map { k =>
+          var sum = 0d
+          var count = 0d
+          if (a.isDefinedAt(k)) {
+            sum += a(k)
+            count += 1
+          }
+          if (b.isDefinedAt(k)) {
+            sum += b(k)
+            count += 1
+          }
+          (k, sum / count)
+        }).toMap
+      }
+      id -> (selfReviews map { case (bId, selfR) =>
+        math.abs(selfR - friendReviews.getOrElse(bId, selfR)) / selfR
+      }).mkString(",")
+    }
+    diffFromFriends.saveAsTextFile(args(1))
+
+
+    //do your friends rate similar to you
+    //does the number of friends you have correlate to how valueful are you as a user
+  }
+
+}
